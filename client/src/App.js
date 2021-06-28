1da11ef6bb8293fa8815a30eb0d00b9f94f36e5c
muxkiller <69748760+muxkiller@users.noreply.github.com>
2021-06-29 00:33:29 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Admin from "./components/admin/Admin.js";
 import NewsManagement from "./components/admin/NewsManagement.js";
 import CreateNews from "./components/admin/CreateNews.js";
+import Landing from './components/layout/Landing'
+import Auth from './views/Auth'
+import AuthContextProvider from './contexts/AuthContext'
 
 
 
@@ -13,6 +16,9 @@ function App() {
         <Route path = "/admin" exact component = {Admin}/>
         <Route path = "/admin/news" exact component = {NewsManagement}/>
         <Route path = "/admin/news/create" exact component = {CreateNews}/>
+        <Route exact path='/' component={Landing}/>
+        <Route exact path='/login' render={props => <Auth {...props} authRoute='login'/>} />
+        <Route exact path='/register' render={props => <Auth {...props} authRoute='register'/>} />
       </Switch>
     </Router>
   );
