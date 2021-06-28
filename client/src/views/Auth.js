695c8e34e8f3e5451def2c82062ea6abc640dc72
muxkiller <69748760+muxkiller@users.noreply.github.com>
2021-06-29 00:51:49 +0700

    Add files via upload

---
+++ client/src/views/Auth.js
@@ -0,0 +1,25 @@
+import LoginForm from '../components/auth/LoginForm'
+import RegisterForm from '../components/auth/RegisterForm'
+
+const Auth = ({ authRoute }) => {
+    let body 
+
+    body = (
+        <>
+            {authRoute === 'login' && <LoginForm />}
+            {authRoute === 'register' && <RegisterForm />}
+        </>
+    )
+
+    return (
+        <div className = "landing">
+            <div className = "dark-overlay">
+                <div className = "landing-inner">
+                    <h1>EarthView</h1>
+                    {body}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Auth
