7e23fb8da7f0c29b2282ebfd6ef2c357a97a8d7c
muxkiller <69748760+muxkiller@users.noreply.github.com>
2021-06-29 00:56:31 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,8 @@
-/* import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Admin from "./components/admin/Admin.js";
 import NewsManagement from "./components/admin/NewsManagement.js";
 import CreateNews from "./components/admin/CreateNews.js";
-import Landing from './components/layout/Landing'
-import Auth from './views/Auth'
-import AuthContextProvider from './contexts/AuthContext'
-
-
-
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path = "/admin" exact component = {Admin}/>
-        <Route path = "/admin/news" exact component = {NewsManagement}/>
-        <Route path = "/admin/news/create" exact component = {CreateNews}/>
-      </Switch>
-    </Router>
-  );
-}
-
-export default App;
-*/
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Landing from './components/layout/Landing'
 import Auth from './views/Auth'
 import AuthContextProvider from './contexts/AuthContext'
@@ -34,6 +12,9 @@ function App() {
     <AuthContextProvider>
         <Router>
           <Switch>
+            <Route path = "/admin" exact component = {Admin}/>
+            <Route path = "/admin/news" exact component = {NewsManagement}/>
+            <Route path = "/admin/news/create" exact component = {CreateNews}/>
             <Route exact path='/' component={Landing}/>
             <Route exact path='/login' render={props => <Auth {...props} authRoute='login'/>} />
             <Route exact path='/register' render={props => <Auth {...props} authRoute='register'/>} />
